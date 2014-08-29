34d5f53013dba10934d7319ee9bab74ddfa05825
pmeijer <patrik85@isis.vanderbilt.edu>
2014-08-29 12:17:09 -0500

    Added return object for function

--- src/compareAdms/admcomparer.js
+++ src/compareAdms/admcomparer.js
@@ -3,9 +3,15 @@ var adm1 = adm2Object('./samples/MyMassSpringDamper.adm');
 var adm2 = adm2Object('./samples/MyMassSpringDamper.adm');
 
 var compareAdms = function (adm1, adm2) {
+    var result = {
+        success: true,
+        messages: []
+    };
     //TODO: here goes the code
     console.log(JSON.stringify(adm1, null, 2));
     console.log(JSON.stringify(adm2, null, 2));
+
+    return result;
 };
 
 compareAdms(adm1, adm2);
