8c1057b6ef2a94c0eba97733854bec4ad7112359
Kevin Smyth <kevin.m.smyth@gmail.com>
2014-12-08 14:35:51 -0600

    Sample app to use ng-gme

--- src/app/sample/app.js
+++ src/app/sample/app.js
@@ -1,5 +1,13 @@
 /*globals angular, console*/
 
+var TIMEOUT = 0;
+// uncomment this and the meta nodes won't be loaded:
+// TIMEOUT = 1000;
+
+var fatal = function (err) {
+    console.log(err);
+};
+
 var CyPhyApp = angular.module('CyPhyApp', [
     'ui.router',
 
@@ -12,9 +20,62 @@ var CyPhyApp = angular.module('CyPhyApp', [
     // app specific templates
     'cyphy.sample.templates'
 ])
-    .run(function () {
-
+    .run(function ($state, $http, growl, dataStoreService, projectService, nodeService) {
+        'use strict';
     });
 
 // TODO: require all of your controllers
-require('./views/MyView/MyViewController');
+//require('./views/MyView/MyViewController');
+
+
+angular.module('CyPhyApp')
+    .controller('MyViewController', function ($scope, $timeout, $http, growl, dataStoreService, projectService, nodeService, branchService) {
+        'use strict';
+
+        $scope.logs = ['init'];
+
+        var log = function (message) {
+            $scope.logs.push(message);
+        };
+
+        var databaseId = 'my-db-connection-id';
+
+        var meta;
+        var context;
+        //$http.get('/rest/external/copyproject/noredirect')
+        //    .then(function(data) {
+        //        //return data.data;
+        //        return "NkLabsPrototype";
+        //    }).then(
+        (function(projectName) {
+                return dataStoreService.connectToDatabase(databaseId, {host: window.location.basename})
+                    .then(function () {
+                        // select default project and branch (master)
+                        log('db open');
+                        return projectService.selectProject(databaseId, projectName);
+                    })
+                    .catch(function (reason) {
+                        log('ADMEditor does not exist. Create and import it using the <a href="' +
+                        window.location.origin + '"> webgme interface</a>.');
+                        fatal(reason);
+                    });
+            })("NkLabsPrototype").then(function (project) {
+                context = {
+                    db: databaseId,
+                    projectId: project,
+                    branchId: 'master',
+                    regionId: (new Date()).toISOString() + 'x'
+                };
+                return $timeout(function () {
+                    return project;
+                }, TIMEOUT);
+            }).then(function () {
+                return branchService.selectBranch(databaseId, "master_copy");
+            }).then(function(project) {
+                return nodeService.getMetaNodes(context);
+            }).then(function (metaNodes) {
+                meta = metaNodes;
+                log('meta loaded');
+                //nodeService.loadNode(context, nodeId)
+            }).catch(fatal);
+    });
